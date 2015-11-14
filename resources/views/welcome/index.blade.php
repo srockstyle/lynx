e53d4db5aaeb881e7681f551f1ce28af07e16b16
srockstyle <kobayashi_shohei@srockstyle.com>
2015-11-15 05:50:31 +0900

    Edit README

--- resources/views/welcome/index.blade.php
+++ resources/views/welcome/index.blade.php
@@ -16,7 +16,7 @@
   </p>
   <footer style="margin:100px auto 20px auto;">
     Main Commiter <a href="https://github.com/srockstyle" target="_blank">srockstyle</a> /
-    <a href="https://github.com/srockstyle/engine-lynx" target="_blank">Engine Lynx</a> is MIT Licence.
+    <a href="https://github.com/srockstyle/engine-lynx" target="_blank">Engine Lynx</a> is open-sourced software licensed under the <a href="http://opensource.org/licenses/MIT" target="_blank">MIT Licence.</a>
   </footer>
 </div>
 </body>
